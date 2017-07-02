3405b246932bdc803a09e1180cde2a5914b2bd0f
fgd-haha <fgd.freedom@gmail.com>
2017-07-02 14:41:55 +0800

    change class out derectory

--- Array_int/src/Main.java
+++ Array_int/src/Main.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
  * Created by hahaha on 2017/6/20.
  */
 public class Main {
-
-
-
+    public static void main(String[] args) {
+        System.out.println("alskdf");
+    }
 }
