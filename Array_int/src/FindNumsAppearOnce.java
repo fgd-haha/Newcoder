72e5337b9ad9796d85c7f40b501487635f21a254
fgd-haha <fgd.freedom@gmail.com>
2017-08-27 21:49:32 +0800

    change class out derectory

--- Array_int/src/FindNumsAppearOnce.java
+++ Array_int/src/FindNumsAppearOnce.java
@@ -4,9 +4,8 @@ import java.util.HashSet;
 /**
  * Created by ha on 4/24/2017.
  * Question:
- *      一个整型数组里除了两个数字之外，其他的数字都出现了两次。请写程序找出这两个只出现一次的数字。
+ * 一个整型数组里除了两个数字之外，其他的数字都出现了两次。请写程序找出这两个只出现一次的数字。
  * Solution:
- *
  */
 //num1,num2分别为长度为1的数组。传出参数
 //将num1[0],num2[0]设置为返回结果
@@ -19,15 +18,17 @@ public class FindNumsAppearOnce {
             array[i + 500000] = i;
         }
         array[500000] = 33333;
+
         int[] num1 = new int[1];
         int[] num2 = new int[1];
-        FindNumsAppearOnce1(array, num1, num2);
+        FindNumsAppearOnce0(array, num1, num2);
         System.out.println(num1[0] + " " + num2[0]);
+
         long endTime = System.nanoTime();
         System.out.println("Time: " + (endTime - startTime));
     }
 
-    public static void FindNumsAppearOnce0(int [] array,int num1[] , int num2[]) {
+    public static void FindNumsAppearOnce0(int[] array, int num1[], int num2[]) {
         num1[0] = -1;
         HashSet<Integer> hashSet = new HashSet<>();
         for (int i : array) {
@@ -39,15 +40,5 @@ public class FindNumsAppearOnce {
             else num2[0] = i;
 
         }
-    }
-
-    public static void FindNumsAppearOnce1(int [] array,int num1[] , int num2[]) {
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        for (int i : array) {
-            if (!arrayList.contains(i)) arrayList.add(i);
-            else arrayList.remove(arrayList.indexOf(i));
-        }
-        num1[0] = arrayList.get(0);
-        num2[0] = arrayList.get(1);
     }
 }
--- Array_int/src/Main.java
+++ Array_int/src/Main.java
@@ -1,10 +1,59 @@
-import java.util.ArrayList;
+ import java.util.*;
 
-/**
- * Created by hahaha on 2017/6/20.
- */
-public class Main {
+
+ public class Main {
     public static void main(String[] args) {
-        System.out.println("alskdf");
+        Scanner in = new Scanner(System.in);
+
+        int color = in.nextInt();
+        int white = in.nextInt();
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int d = in.nextInt();
+        int x = in.nextInt();
+        int y = in.nextInt();
+        int z = in.nextInt();
+
+        double value_ab = (double) x / (a + b);
+        double value_c = (double)y / c;
+        double value_d = (double)z / d;
+
+        TreeMap<String, Double> map = new TreeMap<>();
+        map.put("ab", value_ab);
+        map.put("c", value_c);
+        map.put("d", value_d);
+
+        Comparator<Map.Entry<String, Double>> valueComparator = (o1, o2) -> (o2.getValue().compareTo(o1.getValue()));
+        ArrayList<Map.Entry<String, Double>> list = new ArrayList(map.entrySet());
+        list.sort(valueComparator);
+
+        int value = 0;
+        for (int i = 0; i < 3; i++) {
+            String s = list.get(i).getKey();
+            switch (s) {
+                case "ab": {
+                    int min = Math.min(color / a, white / b);
+                    value += min * x;
+                    color -= min * a;
+                    white -= min * b;
+                    break;
+                }
+                case "c": {
+                    int min = white / c;
+                    value += min * y;
+                    white -= min * c;
+                    break;
+                }
+                case "d": {
+                    int min = color / d;
+                    value += min * z;
+                    color -= min * d;
+                    break;
+                }
+            }
+        }
+
+        System.out.println(value);
     }
-}
+ }
