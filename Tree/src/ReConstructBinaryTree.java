31b6f8cceeff5475aa5e775b948e8cb3d0a56cfa
fgd-haha <fgd.freedom@gmail.com>
2017-09-03 10:37:18 +0800

    update

--- Tree/src/ReConstructBinaryTree.java
+++ Tree/src/ReConstructBinaryTree.java
@@ -8,7 +8,7 @@ public class ReConstructBinaryTree {
         int[] pre = {1, 2, 4, 7, 3, 5, 6, 8};
         int[] in = {4, 7, 2, 1, 5, 3, 8, 6};
         TreeNode tn = reConstructBinaryTree(pre, in);
-        print(tn);
+        tn.midOderPrint();
     }
 
     static TreeNode reConstructBinaryTree(int[] pre, int[] in) {
@@ -31,14 +31,5 @@ public class ReConstructBinaryTree {
         }
 
         return root;
-    }
-
-    private static void print(TreeNode treeNode) {
-        if (treeNode == null) {
-            return;
-        }
-        print(treeNode.left);
-        System.out.print(treeNode.val + " ");
-        print(treeNode.right);
     }
 }
--- Tree/src/TreeNode.java
+++ Tree/src/TreeNode.java
@@ -1,9 +1,52 @@
 public class TreeNode {
     int val;
-    TreeNode left;
-    TreeNode right;
+    TreeNode left = null;
+    TreeNode right = null;
 
     TreeNode(int x) {
         val = x;
+    }
+
+    TreeNode() {
+        TreeNode[] nodes = new TreeNode[5];
+        nodes[0] = new TreeNode(10);
+        nodes[1] = new TreeNode(5);
+        nodes[2] = new TreeNode(12);
+        nodes[3] = new TreeNode(4);
+        nodes[4] = new TreeNode(7);
+        nodes[1].left = nodes[3];
+        nodes[1].right = nodes[4];
+        this.val = nodes[0].val;
+        this.left = nodes[1];
+        this.right = nodes[2];
+    }
+
+    public static void main(String[] args) {
+        TreeNode node = new TreeNode(3);
+        node.preOderPrint();
+    }
+
+    void preOderPrint() {
+        System.out.print(this.val + " ");
+        if (this.left != null)
+            this.left.preOderPrint();
+        if (this.right != null)
+            this.right.preOderPrint();
+    }
+
+    void midOderPrint() {
+        if (this.left != null)
+            this.left.midOderPrint();
+        System.out.print(this.val + " ");
+        if (this.right != null)
+            this.right.midOderPrint();
+    }
+
+    void afterOderPrint() {
+        if (this.left != null)
+            this.left.afterOderPrint();
+        if (this.right != null)
+            this.right.afterOderPrint();
+        System.out.print(this.val + " ");
     }
 }
