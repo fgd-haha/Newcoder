dff690ee795bccc990cd2ba3ae506dd20af0d082
fgd-haha <fgd.freedom@gmail.com>
2017-09-09 00:04:09 +0800

    create

--- Array_int/src/Test.java
+++ Array_int/src/Test.java
@@ -1,18 +1,1 @@
-import java.util.HashSet;
-import java.util.Set;
-
-public class Test {
-    public static void main(String[] args) {
-        HashSet<Integer> s1 = new HashSet<>();
-        HashSet<Integer> s2 = new HashSet<>();
-        s1.add(1);
-        s2.add(2);
-        s1.add(2);
-        s2.add(1);
-        HashSet<HashSet<Integer>> set = new HashSet<>();
-        set.add(s1);
-        set.add(s2);
-
-        System.out.println(set);
-    }
-}
+// 冷静，专注
